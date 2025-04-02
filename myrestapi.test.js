2eacf87a20c4fab2cdec01038380d611e38b7570
Watsawee <watsawee@gmail.com>
2025-04-02 11:56:32 +0700

    add 2 more test cases

--- myrestapi.test.js
+++ myrestapi.test.js
@@ -4,4 +4,10 @@ describe('test hr zone calculation', () => {
   test('test - zone number is correct?', () => {
     expect(restapi.calculateHR(30,180).zone).toBe(5);
   });
+  test('test - lb hr is correct?', () => {
+    expect(restapi.calculateHR(30,180).lb).toBe(171);
+  });
+  test('test - ub hr is correct?', () => {
+    expect(restapi.calculateHR(30,180).ub).toBe(190);
+  });
 });
